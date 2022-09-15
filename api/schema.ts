f8dd0fe6202c5977b83e0659601aca9772762783
sangyouh <sangyouh@usc.edu>
2022-09-15 15:41:38 +0900

    next playground first commit

--- api/schema.ts
+++ api/schema.ts
@@ -2,9 +2,9 @@ import { makeSchema } from "nexus";
 import { join } from "path";
 
 export const schema = makeSchema({
-    types: [],
-    outputs: {
-        typegen: join(__dirname, "..", "nexus-typegen.ts"),
-        schema: join(__dirname, "..", "schema.graphql"),
-    }
+  types: [],
+  outputs: {
+    typegen: join(__dirname, "..", "nexus-typegen.ts"),
+    schema: join(__dirname, "..", "schema.graphql"),
+  },
 });
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import "./moduleAliases";
+
+export {};
